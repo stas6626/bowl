bb3a1da3d91bf5a9bc2b4f7ccf35e695dca26d89
Stanislav <doga05@bk.ru>
2018-04-10 20:21:43 +0300

    Add files via upload

--- utils_and_losses.py
+++ utils_and_losses.py
@@ -4,7 +4,7 @@ import torch
 from skimage.morphology import label
 from torch import nn
 from scipy import ndimage as ndi
-
+import torch.nn.functional as F
 
 class LossBinary:
     def __init__(self, jaccard_weight=0):
@@ -29,3 +29,36 @@ class LossBinary:
 def to_float_tensor(img):
     return torch.from_numpy(np.moveaxis(img, -1, 0)).float()
 
+
+def default_loss_composition(loss1, loss2, loss3):
+    return loss1 + loss2 + loss3
+
+
+def iou_t(target, pred):
+    target_y = label(target > 0.5)
+    pred_y = label(pred > 0.5)
+    true_objs = len(np.unique(target_y))
+    pred_objs = len(np.unique(pred_y))
+    intersection = np.histogram2d(target_y.flatten(),
+                                pred_y.flatten(),
+                                bins=(true_objs, pred_objs))[0]
+  
+    area_true = np.histogram(target_y, bins = true_objs)[0]
+    area_pred = np.histogram(pred_y, bins = pred_objs)[0]
+    area_true = np.expand_dims(area_true, -1)
+    area_pred = np.expand_dims(area_pred, 0)
+
+    union = area_true + area_pred - intersection
+
+    intersection = intersection[1:,1:]
+    union = union[1:,1:]
+    union[union == 0] = 1e-9
+
+    iou = intersection / union
+
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        tp, fp, fn = precision_at(t, iou)
+        p = tp / (tp + fp + fn)
+        prec.append(p)
+    return np.mean(prec)
